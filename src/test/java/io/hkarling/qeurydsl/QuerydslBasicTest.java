e306a38eca18bf2a63c6f969ec01270d075e3295
Chamsol Yoon <dantert@gmail.com>
2022-01-26 23:40:49 +0900

    기본 문법 2

--- src/test/java/io/hkarling/qeurydsl/QuerydslBasicTest.java
+++ src/test/java/io/hkarling/qeurydsl/QuerydslBasicTest.java
@@ -1,15 +1,17 @@
 package io.hkarling.qeurydsl;
 
 import static io.hkarling.qeurydsl.entity.QMember.*;
+import static io.hkarling.qeurydsl.entity.QTeam.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import io.hkarling.qeurydsl.entity.Member;
-import io.hkarling.qeurydsl.entity.QMember;
 import io.hkarling.qeurydsl.entity.Team;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -87,5 +89,209 @@ public class QuerydslBasicTest {
             .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch() {
+        // List
+        List<Member> fetch = queryFactory
+            .select(member)
+            .fetch();
+
+        // 단 건, 결과가 둘 이상이면 NonUniqueResultException
+        Member fetchOne = queryFactory
+            .selectFrom(member)
+            .fetchOne();
+
+        // 처음 한 건 조회
+        Member fetchFirst = queryFactory
+            .selectFrom(member)
+            .fetchFirst();
+
+        // 페이징에서 사용
+        QueryResults<Member> memberQueryResults = queryFactory
+            .selectFrom(member)
+            .fetchResults();
+
+        // 카운트쿼리
+        long count = queryFactory
+            .selectFrom(member)
+            .fetchCount();
+    }
+
+    /**
+     * 멤버 정렬 순서
+     * 1. 나이 순서 (desc)
+     * 2. 이름 순서 (asc)
+     * 단, 이름이 없으면 마지막에 출력
+     */
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> members = queryFactory.selectFrom(member)
+            .where(member.age.eq(100))
+            .orderBy(member.age.desc(), member.username.asc().nullsLast())
+            .fetch();
+    }
+
+    @Test
+    public void paging1() {
+        List<Member> result = queryFactory
+            .selectFrom(member)
+            .orderBy(member.username.asc())
+            .offset(1)
+            .limit(2)
+            .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void paging2() {
+        QueryResults<Member> queryResults = queryFactory
+            .selectFrom(member)
+            .orderBy(member.username.asc())
+            .offset(1)
+            .limit(2)
+            .fetchResults(); // fetchResults() 는 deprecated 되었으므로 fetch()를 사용하고, count 쿼리를 분리해서 사용한다.
+
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
+
+    /**
+     * JPQL
+     * select
+     *   COUNT(m), //회원수
+     *   SUM(m.age), //나이 합
+     *   AVG(m.age), //평균 나이
+     *   MAX(m.age), //최대 나이
+     *   MIN(m.age) //최소 나이
+     * from Member m
+     */
+    @Test
+    public void aggregation() {
+        List<Tuple> result = queryFactory
+            .select(
+                member.count(),
+                member.age.sum(),
+                member.age.avg(),
+                member.age.max(),
+                member.age.min())
+            .from(member)
+            .fetch();
+
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    }
+
+    /**
+     * 팀의 이름과 각 팀의 평균 연령을 구해라
+     */
+    @Test
+    public void group() {
+        List<Tuple> result = queryFactory.select(team.name, member.age.avg())
+            .from(member)
+            .join(member.team, team)
+            .groupBy(team.name)
+            //.having() 여기다 넣는다.
+            .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
+
+    /**
+     * Team A에 소속된 모든 멤버
+     */
+    @Test
+    public void join() {
+        List<Member> result = queryFactory.selectFrom(member)
+            .join(member.team, team)
+            .where(team.name.eq("teamA"))
+            .fetch();
+
+        assertThat(result).extracting("username").containsExactly("member1", "member2");
+    }
+
+    @Test
+    public void join2() {
+        List<Member> result = queryFactory.selectFrom(member)
+            .leftJoin(member.team, team)
+            .where(team.name.eq("teamA"))
+            .fetch();
+
+        assertThat(result).extracting("username").containsExactly("member1", "member2");
+    }
+
+    /**
+     * 세타 조인(연관관계가 없는 필드로 조인)
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     */
+    @Test
+    public void thetaJoin() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory.select(member)
+            .from(member, team)
+            .where(member.username.eq(team.name))
+            .fetch();
+
+        assertThat(result).extracting("username").containsExactly("teamA", "teamB");
+    }
+
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL: SELECT m, t FROM Member m LEFT JOIN m.team t on t.name = 'teamA'
+     *  SQL: SELECT m.*, t.* FROM Member m LEFT JOIN Team t ON m.TEAM_ID=t.id and t.name='teamA'
+     */
+    @Test
+    public void joinOnFiltering() {
+        List<Tuple> result = queryFactory.select(member, team)
+            .from(member)
+            .leftJoin(member.team, team).on(team.name.eq("teamA"))
+            .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println(tuple);
+        }
+    }
+
+    /**
+     * 연관관계가 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     */
+    @Test
+    public void joinOnNoRelation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory.select(member, team)
+            .from(member)
+            .leftJoin(team).on(member.username.eq(team.name))
+            //.join(team).on(member.username.eq(team.name))
+            .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println(tuple);
+        }
     }
 }
